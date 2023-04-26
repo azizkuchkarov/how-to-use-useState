3afec6290727f75450716997d96a0d71665a3473
azizkuchkarov <aziz_kuchkarov91@mail.ru>
2023-04-26 10:12:33 +0500

    How to use useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const Class = ({ lastName }) => {
+  const [age, setAge] = useState(0);
+  const [isLogin, setIslogin] = useState(false);
+
+  const heLlo = () => {
+    setAge(age + 1);
+  };
+  const noHello = () => {
+    setAge(age - 1);
+  };
+  const Toggle = () => {
+    setIslogin((prevState) => !prevState);
+  };
+  return (
+    <div>
+      <h1>hello {lastName}</h1>
+      <button className="btn btn-danger" onClick={heLlo}>
+        +
+      </button>
+      <button className="btn btn-warning m-3" onClick={noHello}>
+        -
+      </button>
+      <p className="text-justify-center">{age}</p>
+      <div>{isLogin ? <p>Login</p> : null}</div>
+      <button className="btn btn-outline-primary" onClick={Toggle}>
+        Login
+      </button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Class lastName="React" />
+      <Class lastName="Native" />
     </div>
   );
 }
